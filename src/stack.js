9a54da413d42637d8977dc470de6010d6b119406
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2022-04-29 18:26:33 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.stack=[]
   }
-
+  push(e) {
+    return this.stack.push(e);
+  }
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop();
   }
-
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
-}
-
+};
 module.exports = {
   Stack
 };
